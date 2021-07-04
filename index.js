dfbcd34f3d818ce814e42bf078a5fcd1f293e1c4
vijay kumar vemana <vijaykumarvemana18@gmail.com>
2021-07-04 09:23:39 +0200

    checking user input empty or not

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const btn = document.getElementById('button')
 btn.onclick =  function (){
     const inputValue = document.getElementById('input').value
-    const h1 = document.querySelector('h1')  
-    const title = document.getElementsByTagName('title') 
-    title.innerHTML = `${inputValue}`
-    h1.innerHTML = `${inputValue}`
+    if(inputValue.length == 0){
+        alert("Empty field")
+    }else{
+        const h1 = document.querySelector('h1')  
+        const title = document.getElementsByTagName('title') 
+        title.innerHTML = `${inputValue}`
+        h1.innerHTML = `${inputValue}`
+
+    }
+    
 }
